0fea5cb3b9270ee7ba3cd8459f340fc6735db3a1
Petter Arvidsson <petter.arvidsson@gmail.com>
2022-11-13 11:11:13 +0100

    Works with display board

--- pio_display/pio_display.c
+++ pio_display/pio_display.c
@@ -26,11 +26,64 @@ static uint32_t shift40[] = {
   2,2,2,2,2,2,2,2
 };
 
+static uint32_t initialize[] = {
+  1 << 16, 0x8d14afe3,
+  1 << 16, 0xe3B00210,
+  (32 << 16) | 1,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  1 << 16, 0xe3B10210,
+  (32 << 16) | 1,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  1 << 16, 0xe3B20210,
+  (32 << 16) | 1,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  1 << 16, 0xe3B30210,
+  (32 << 16) | 1,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  1 << 16, 0xe3B40210,
+  (32 << 16) | 1,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  1 << 16, 0xe3B50210,
+  (32 << 16) | 1,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  1 << 16, 0xe3B60210,
+  (32 << 16) | 1,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  1 << 16, 0xe3B70210,
+  (32 << 16) | 1,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000,
+  0x00000000, 0x00000000, 0x00000000, 0x00000000 ,0x00000000, 0x00000000, 0x00000000, 0x00000000
+};
+
 static uint dma_init(PIO pio, uint sm) {
   int channel = dma_claim_unused_channel(true);
 
   dma_channel_config channel_config = dma_channel_get_default_config(channel);
   channel_config_set_dreq(&channel_config, pio_get_dreq(pio, sm, true));
+  channel_config_set_transfer_data_size(&channel_config, DMA_SIZE_32);
   dma_channel_configure(channel,
                         &channel_config,
                         &pio->txf[sm],
@@ -51,10 +104,6 @@ void pio_display_init() {
   gpio_set_dir(CS, GPIO_OUT);
   gpio_put(CS, 1);
 
-  // Initialize shift register with 1s
-  dma_channel_transfer_from_buffer_now(channel, shift40, sizeof(shift40) / sizeof(*shift40));
-  dma_channel_wait_for_finish_blocking(channel);
-
   pio = pio0;
 
   uint offset = pio_add_program(pio, &spi_program);
@@ -63,17 +112,25 @@ void pio_display_init() {
   spi_program_init(pio, sm, offset, MOSI, DC, SCLK);
 
   channel = dma_init(pio, sm);
+
+  // Initialize displays all at once
+  dma_channel_transfer_from_buffer_now(channel, initialize, sizeof(initialize) / sizeof(*initialize));
+  dma_channel_wait_for_finish_blocking(channel);
+
+  // Initialize shift register with 1s
+  // dma_channel_transfer_from_buffer_now(channel, shift40, sizeof(shift40) / sizeof(*shift40));
+  // dma_channel_wait_for_finish_blocking(channel);
 }
 
 void pio_display_update(const uint32_t * const data, const size_t size) {
-    // Activate first display
-    gpio_put(CS, 0);
-    dma_channel_transfer_from_buffer_now(channel, shift1, sizeof(shift1) / sizeof(*shift1));
-    dma_channel_wait_for_finish_blocking(channel);
+  // Activate first display
+  // gpio_put(CS, 0);
+  // dma_channel_transfer_from_buffer_now(channel, shift1, sizeof(shift1) / sizeof(*shift1));
+  // dma_channel_wait_for_finish_blocking(channel);
 
-    // Push data to all displays
-    gpio_put(CS, 1);
-    dma_channel_transfer_from_buffer_now(channel, data, size);
+  // Push data to all displays
+  /// gpio_put(CS, 1);
+  dma_channel_transfer_from_buffer_now(channel, data, size);
 }
 
 void pio_display_wait_for_finish_blocking() {
